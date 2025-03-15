01a6ceb8d92da26958195c78eb7223ebed2cc972
Eli Front <elifront23@gmail.com>
2025-03-14 20:52:36 -0500

    fix: update uuid import to stay consistent and work with new version

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,16 @@
-
 export default {
-	input: 'dist-transpiled/index.js',
-	external: [
-		'path',
-		'crypto',
-		'rollup-plugin-inject'
-	],
-	output: [
-		{
-			format: 'es',
-			file: 'dist/index.mjs',
-			preferConst: true,
-		},
-		{
-			format: 'cjs',
-			file: 'dist/index.js',
-			preferConst: true,
-		}
-	]
+  input: "dist-transpiled/index.js",
+  external: ["path", "crypto", "@rollup/plugin-inject"],
+  output: [
+    {
+      format: "es",
+      file: "dist/index.mjs",
+      preferConst: true,
+    },
+    {
+      format: "cjs",
+      file: "dist/index.js",
+      preferConst: true,
+    },
+  ],
 };
